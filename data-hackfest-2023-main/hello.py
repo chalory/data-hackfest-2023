9968d59cdca19a230f94e1983c779bd13e541708
Clafted <noahm.p.in2017@gmail.com>
2023-10-14 17:24:01 -0400

    Access to form response data at request.form (it's a dictionary)

--- data-hackfest-2023-main/hello.py
+++ data-hackfest-2023-main/hello.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def hello():
+    if(request.method == "POST"):
+        print(request.form)
     return render_template("index.html")
